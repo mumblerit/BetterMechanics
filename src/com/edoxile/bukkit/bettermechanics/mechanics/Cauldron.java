a2b74c8fafffc427b6935fcbeae89efbfe14bc5b
Koen Scheres <koen_75_@hotmail.com>
2011-06-17 16:13:32 +0200

    Cauldron now supports non-recipe glass as floor

--- src/com/edoxile/bukkit/bettermechanics/mechanics/Cauldron.java
+++ src/com/edoxile/bukkit/bettermechanics/mechanics/Cauldron.java
@@ -9,6 +9,7 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import javax.swing.plaf.TreeUI;
 import java.util.*;
 
 /**
@@ -18,7 +19,7 @@ import java.util.*;
  */
 public class Cauldron {
     public static Cauldron preCauldron(Block clickedBlock, MechanicsConfig c, Player p) {
-        if(!c.getCauldronConfig().enabled)
+        if (!c.getCauldronConfig().enabled)
             return null;
         Block tempBlock;
         int dx, dy, dz;
@@ -48,12 +49,12 @@ public class Cauldron {
             //We don't want the lava in our recipe
             dy++;
             HashSet<Block> blockSet = new HashSet<Block>();
-            Block referencePoint = clickedBlock.getRelative(dx,dy,dz);
+            Block referencePoint = clickedBlock.getRelative(dx, dy, dz);
             for (int ndy = 0; ndy <= 3; ndy++) {
                 for (int ndx = 0; ndx <= 1; ndx++) {
                     for (int ndz = 0; ndz <= 1; ndz++) {
-                        if(referencePoint.getRelative(ndx, ndy, ndz).getType() != Material.AIR){
-                            blockSet.add(referencePoint.getRelative(ndx,ndy,ndz));
+                        if (referencePoint.getRelative(ndx, ndy, ndz).getType() != Material.AIR) {
+                            blockSet.add(referencePoint.getRelative(ndx, ndy, ndz));
                         }
                     }
                 }
@@ -75,12 +76,23 @@ public class Cauldron {
 
     public boolean performCauldron() {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> noGlassMap = new HashMap<Integer, Integer>();
 
         for (Block b : contents) {
-            if (!map.containsKey(b.getTypeId())) {
-                map.put(b.getTypeId(), 1);
+            if (b.getTypeId() == Material.GLASS.getId()) {
+                if (!map.containsKey(b.getTypeId())) {
+                    map.put(b.getTypeId(), 1);
+                } else {
+                    map.put(b.getTypeId(), map.get(b.getTypeId()) + 1);
+                }
             } else {
-                map.put(b.getTypeId(), map.get(b.getTypeId()) + 1);
+                if (!map.containsKey(b.getTypeId())) {
+                    noGlassMap.put(b.getTypeId(), 1);
+                    map.put(b.getTypeId(), 1);
+                } else {
+                    noGlassMap.put(b.getTypeId(), noGlassMap.get(b.getTypeId()) + 1);
+                    map.put(b.getTypeId(), map.get(b.getTypeId()) + 1);
+                }
             }
         }
 
@@ -91,6 +103,11 @@ public class Cauldron {
 
             player.sendMessage(ChatColor.GOLD + "In a proof of smoke, you've made " + recipe.getName() + ".");
 
+            for (Block b : contents) {
+                if (isDependant(b.getTypeId())) {
+                    b.setType(Material.AIR);
+                }
+            }
             for (Block b : contents) {
                 b.setType(Material.AIR);
             }
@@ -105,8 +122,66 @@ public class Cauldron {
             player.updateInventory();
             return true;
         } else {
-            player.sendMessage(ChatColor.RED + "Hmm, this doesn't make anything...");
-            return false;
+            //Check if glassless recipe exists:
+            recipe = recipes.find(noGlassMap);
+            if (recipe != null) {
+
+                player.sendMessage(ChatColor.GOLD + "In a proof of smoke, you've made " + recipe.getName() + ".");
+
+                for (Block b : contents) {
+                    if (b.getTypeId() != Material.GLASS.getId() && isDependant(b.getTypeId())) {
+                        b.setType(Material.AIR);
+                    }
+                }
+                for (Block b : contents) {
+                    if (b.getTypeId() != Material.GLASS.getId())
+                        b.setType(Material.AIR);
+                }
+
+                // Give results
+                for (Integer id : recipe.getResults()) {
+                    HashMap<Integer, ItemStack> inventoryMap = player.getInventory().addItem(new ItemStack(id, 1));
+                    for (Map.Entry<Integer, ItemStack> i : inventoryMap.entrySet()) {
+                        player.getLocation().getWorld().dropItem(player.getLocation(), i.getValue());
+                    }
+                }
+                player.updateInventory();
+                return true;
+            } else {
+                player.sendMessage(ChatColor.RED + "Hmm, this doesn't make anything...");
+                return false;
+            }
+        }
+    }
+
+    private boolean isDependant(int itemId) {
+        switch (itemId) {
+            case 6:
+            case 31:
+            case 32:
+            case 37:
+            case 38:
+            case 39:
+            case 40:
+            case 50:
+            case 51:
+            case 55:
+            case 59:
+            case 69:
+            case 70:
+            case 71:
+            case 72:
+            case 75:
+            case 76:
+            case 78:
+            case 81:
+            case 83:
+            case 93:
+            case 94:
+            case 96:
+                return true;
+            default:
+                return false;
         }
     }
 }
